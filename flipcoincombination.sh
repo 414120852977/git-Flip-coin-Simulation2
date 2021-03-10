7b59b07afa031abe7f7288538294bd14a84e6994
Ashok Mane <ashokmane81800@gmail.com>
2021-03-10 11:18:31 +0530

    [ashok] created singlet head or tail

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,13 +1,36 @@
 #! /bin/bash -x
 head=0
 tail=1
-for(( i=0; i<10; i++ ))
+for(( i=0; i<20; i++ ))
 do
 	s=$(( RANDOM%2 ))
+	echo ${s[@]}
 	if [ $(( s )) -eq 0 ]
 	then 
 		echo "its flip head "$s
 	else
 		echo "its a tail" $S
 	fi
+done 
+H=0
+T=1
+
+for(( i=0; i<20; i++ ))
+do
+	s=$(( RANDOM%H ))
+	 echo s[$i]
+done
+
+echo ${s[$i]}
+
+if [ $(( s )) -eq 0 ]
+then
+	echo "H"
+else 
+echo "T"
+fi
+
+for index in ${!s[@]}
+do
+	echo $index  "is " ${s[@]}
 done
